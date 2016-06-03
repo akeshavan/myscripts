64eb71ef19d8c26be46f8008e062c6ec01896ef2
kesshijordan <Kesshi.Jordan@ucsf.edu>
2016-06-03 15:59:48 -0700

    bug fix

--- Distortion_Pipeline/run_apmwarp_pipe.py
+++ Distortion_Pipeline/run_apmwarp_pipe.py
@@ -3,9 +3,10 @@ from glob import glob
 from create_apm_warp2t1 import nonlinear_reg_diff2t2_workflow
 from subprocess import call
 
-subfile = '/home/kjordan/python_code/mydata_4myscripts/darpa'
+subfile = '/home/kjordan/python_code/mydata_4myscripts/darpa/ptlist_darpa.txt'
 with open(subfile) as f:
-    ptlist = f.read().splitlines()
+    lines = f.read().splitlines()
+ptlist = map(str.strip, lines[0].split(','))
 
 datadir = '/data/henry7/PBR/subjects'
 workingdir = '/scratch/henry_temp/kesshi/CHANGLAB/new_working_dir4'
